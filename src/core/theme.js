aa3e054c78f8cf97535ec307661a56b1ae6019b9
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-04-24 14:11:27 +0200

    added more colors to theme

--- src/core/theme.js
+++ src/core/theme.js
@@ -10,6 +10,10 @@ export const theme = {
     mainBlack: "rgba(24, 24, 27, 1)",
     mainWhite: "rgba(255, 255, 255, 1)",
     darkerGray: "rgba(126, 131, 154, 1)",
+    mystic: "#E4E6F0",
+    woodsmoke: "#18181B",
+    pattensBlue: "#D6E4FF",
+    waterloo: "#7E839A",
   }
 
 }
