913bb47f0922373e0de6ecd3f4e19e7c6f7d9bfb
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-04-26 01:41:41 +0200

    imagesAPI added

--- src/features/moviesPage/index.js
+++ src/features/moviesPage/index.js
@@ -1,25 +1,26 @@
 import { useEffect } from "react";
 import { fetchPopularMovies, selectPopularMoviesList, selectPopularMoviesStatus } from "./popularMoviesSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { imagesAPI600x900 } from "../../core/API";
 
 export const MoviesPage = () => {
-const dispatch = useDispatch();
-useEffect(()=> {
-  dispatch(fetchPopularMovies())
-}, [])
-const status = useSelector(selectPopularMoviesStatus);
-const a = useSelector(selectPopularMoviesList);
-if(status === "success")
-{
-console.log(a.results)
-return(
-  <>
-<div><ul>
-  {a.results.map(movie => (
-<li key={movie.id}>{movie.title}</li>
-  ))}</ul>
-  </div>
-</>
-);
-}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPopularMovies())
+  }, [])
+  const status = useSelector(selectPopularMoviesStatus);
+  const a = useSelector(selectPopularMoviesList);
+  if (status === "success") {
+    console.log(a.results)
+    return (
+      <>
+        <div><ul>
+          {a.results.map(movie => (
+            <li key={movie.id}>{movie.title}<img src={`${imagesAPI600x900}${movie.poster_path}`} alt=""></img>
+            </li>
+          ))}</ul>
+        </div>
+      </>
+    );
+  }
 };
