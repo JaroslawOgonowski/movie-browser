93fb14588ee2f8ecc95187e02ce4f2888cfe190b
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-04-26 16:03:36 +0200

    added layout

---
+++ src/features/moviesPage/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+    margin: 24px auto 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 24px;
+    justify-content: center;
+`;
