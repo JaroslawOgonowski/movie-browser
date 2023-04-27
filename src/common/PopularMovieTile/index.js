75feaa06e1b1a33ce4a57eaeab704bdbc812e477
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-04-28 01:25:17 +0200

    poster debug

--- src/common/PopularMovieTile/index.js
+++ src/common/PopularMovieTile/index.js
@@ -2,7 +2,7 @@ import { ButtonTag, Description, InfoWrapper, MovieTitle, Poster, Rate, Rating, 
 import star from "../images/star.svg";
 import { useSelector } from "react-redux";
 import { selectGenres } from "../../features/moviesPage/Genres/genresSlice";
-import placeholder from "./poster.svg"
+import placeholder from "./Poster.jpg"
 
 const PopularMovieTile = ({ poster, title, date, rate, voteCount, genres }) => {
     const genresArray = useSelector(selectGenres);
@@ -10,7 +10,7 @@ const PopularMovieTile = ({ poster, title, date, rate, voteCount, genres }) => {
 
     return (
         <TileWrapper>
-            <Poster src={poster ? poster : placeholder}>
+            <Poster src={poster === "https://image.tmdb.org/t/p/w600_and_h900_bestv2/null" ? placeholder : poster}>
             </Poster>
             <InfoWrapper>
                 <Description>
