bf6783ddabdaeed6d84e6ca067b107882c377ed5
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-04-28 10:33:43 +0200

    add title to popular movies

--- src/features/moviesPage/styled.js
+++ src/features/moviesPage/styled.js
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
+export const Title = styled.h2`
+  margin-top: 56px;
+  margin-bottom: 24px;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+
+  @media(max-width: ${({ theme }) => theme.breakPoints.mobileMax}px) {
+    margin-top: 24px;
+    margin-bottom: 12px;
+    font-size: 20px;
+  };
+`;
+
 export const Layout = styled.div`
     margin: 24px auto 40px;
     display: grid;
