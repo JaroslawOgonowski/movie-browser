39f0cfc003465d607fae2fd72875e3459d2d5417
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-05-03 11:14:28 +0200

    titles conditioning

--- src/features/peoplePage/personPage/index.js
+++ src/features/peoplePage/personPage/index.js
@@ -16,28 +16,50 @@ export const PersonPage = () => {
       dispatch(fetchPersonById(id))
     }
   }, [dispatch, id]);
-  
+
   if (status === "error") return <ErrorPage />
   if (status === "loading") return <Loader searchFor={"person"} />
   if (status === "success")
-  return(
-    <>
-    <div>{personInfo.personDescription.name}</div>
-    <div>{personInfo.personDescription.biography}</div>
-    <h2>Movies- cast</h2>
-  <div>{personInfo.personMovies.cast.map(movie =>
-    <MovieTile
-    key={movie.id}
-    id={movie.id}
-    poster={movie.poster_path}
-    title={movie.original_title}
-    date={movie.release_date}
-    rate={movie.vote_average}
-    voteCount={movie.vote_count}
-    genres={movie.genre_ids}
+    return (
+      <>
+        <div>{personInfo.personDescription.name}</div>
+        <div>{personInfo.personDescription.biography}</div>
+        {personInfo.personMovies.cast.length === 0 ?
+          null :
+          <>
+            <h2>Movies- cast ({personInfo.personMovies.cast.length})</h2>
+            <div>{personInfo.personMovies.cast.map(movie =>
+              <MovieTile
+                key={movie.id}
+                id={movie.id}
+                poster={movie.poster_path}
+                title={movie.original_title}
+                date={movie.release_date}
+                rate={movie.vote_average}
+                voteCount={movie.vote_count}
+                genres={movie.genre_ids}
 
-    />
-    )}</div>
-    </>
-  )
+              />
+            )}</div></>
+        }
+        {personInfo.personMovies.crew.length === 0 ?
+          null :
+          <>
+            <h2>Movies- crew ({personInfo.personMovies.crew.length})</h2>
+            <div>{personInfo.personMovies.crew.map(movie =>
+              <MovieTile
+                key={movie.id}
+                id={movie.id}
+                poster={movie.poster_path}
+                title={movie.original_title}
+                date={movie.release_date}
+                rate={movie.vote_average}
+                voteCount={movie.vote_count}
+                genres={movie.genre_ids}
+
+              />
+            )}</div></>
+        }
+      </>
+    )
 }
