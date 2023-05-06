ac6ac73dd7f1c39768b5f0d76a4fcae9837d73f4
Szymon Mlynski <thxsymek@gmail.com>
2023-05-06 08:08:53 +0200

    added profilePage but not responsive yet

--- src/features/peoplePage/personPage/index.js
+++ src/features/peoplePage/personPage/index.js
@@ -6,16 +6,14 @@ import Loader from "../../../common/Loader";
 import MovieTile from "../../../common/MovieTile";
 import { useQueryParameters } from "../../search/queryParameters";
 import { Layout } from "../../moviesPage/styled";
-import { selectSearchPeopleStatus } from "../../search/searchSlice";
-import { SearchPeoplePage } from "../../search/searchPeoplePage";
+import { Container } from "../../../common/Container";
+import ProfilePage from "./ProfilePage";
 
 export const PersonPage = () => {
-  const id = useQueryParameters("id");
-  const query = useQueryParameters("search");
-  const personInfo = useSelector(selectPersonInfo);
-  const dispatch = useDispatch();
-  const status = useSelector(selectPersontatus);
-  const searchStatus = useSelector(selectSearchPeopleStatus);
+  const id = useQueryParameters("id")
+  const personInfo = useSelector(selectPersonInfo)
+  const dispatch = useDispatch()
+  const status = useSelector(selectPersontatus)
 
   useEffect(() => {
     if (id) {
@@ -23,50 +21,56 @@ export const PersonPage = () => {
     }
   }, [dispatch, id]);
 
-  if (status === "error" && query === null) return <ErrorPage />
-  if (status === "loading" && query === null) return <Loader searchFor={"person"} />
-  if (searchStatus === "success" && query !== null) return <SearchPeoplePage />
-  if (status === "success" && query === null)
+  if (status === "error") return <ErrorPage />
+  if (status === "loading") return <Loader searchFor={"person"} />
+  if (status === "success")
     return (
       <>
-        <div>{personInfo.personDescription.name}</div>
-        <div>{personInfo.personDescription.biography}</div>
-        {personInfo.personMovies.cast.length === 0 ?
-          null :
-          <>
-            <h2>Movies- cast ({personInfo.personMovies.cast.length})</h2>
-            <Layout>{personInfo.personMovies.cast.map(movie =>
-              <MovieTile
-                key={`${movie.id}${movie.character}`}
-                id={movie.id}
-                poster={movie.poster_path}
-                title={movie.original_title}
-                date={movie.release_date}
-                rate={movie.vote_average}
-                voteCount={movie.vote_count}
-                genres={movie.genre_ids}
+        <Container>
+          <ProfilePage
+            poster={personInfo.personDescription.profile_path}
+            name={personInfo.personDescription.name}
+            birthday={personInfo.personDescription.birthday}
+            birthplace={personInfo.personDescription.place_of_birth}
+            biography={personInfo.personDescription.biography}
+          />
+          {personInfo.personMovies.cast.length === 0 ?
+            null :
+            <>
+              <h2>Movies- cast ({personInfo.personMovies.cast.length})</h2>
+              <Layout>{personInfo.personMovies.cast.map(movie =>
+                <MovieTile
+                  key={movie.id}
+                  id={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.original_title}
+                  date={movie.release_date}
+                  rate={movie.vote_average}
+                  voteCount={movie.vote_count}
+                  genres={movie.genre_ids}
 
-              />
-            )}</Layout></>
-        }
-        {personInfo.personMovies.crew.length === 0 ?
-          null :
-          <>
-            <h2>Movies- crew ({personInfo.personMovies.crew.length})</h2>
-            <Layout>{personInfo.personMovies.crew.map(movie =>
-              <MovieTile
-                key={`${movie.id}${movie.job}`}
-                id={movie.id}
-                poster={movie.poster_path}
-                title={movie.original_title}
-                date={movie.release_date}
-                rate={movie.vote_average}
-                voteCount={movie.vote_count}
-                genres={movie.genre_ids}
+                />
+              )}</Layout></>
+          }
+          {personInfo.personMovies.crew.length === 0 ?
+            null :
+            <>
+              <h2>Movies- crew ({personInfo.personMovies.crew.length})</h2>
+              <Layout>{personInfo.personMovies.crew.map(movie =>
+                <MovieTile
+                  key={movie.id}
+                  id={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.original_title}
+                  date={movie.release_date}
+                  rate={movie.vote_average}
+                  voteCount={movie.vote_count}
+                  genres={movie.genre_ids}
 
-              />
-            )}</Layout></>
-        }
+                />
+              )}</Layout></>
+          }
+        </Container>
       </>
     )
 }
