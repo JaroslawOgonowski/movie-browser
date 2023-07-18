bc71863e9aa7acf16da111b82d11104d3f41b2d1
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-07-18 23:43:09 +0200

    add padding bottom to container

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Container = styled.main`
     margin: 0 auto;
     max-width: 1368px;
-
+    padding-bottom: 50px;
+    
     @media (max-width: ${({ theme }) => theme.breakPoints.fullPage}px) {
         margin: 0 16px;
     };
--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { BackwardArrow, ForwardArrow } from "./buttonArrows";
 import { Button, ButtonText, PageNumber, StyledButtons, StyledPages, StyledPagination } from "./styled";
 import { fetchPopularPeople } from "../../features/peoplePage/popularPeopleSlice";
